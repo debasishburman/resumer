2f62dcb934422d4d02caeb1c6cf3f625a1af719e
debasishburman <167029485+debasishburman@users.noreply.github.com>
2024-04-15 10:27:54 +0530

    Update skillmatcher.py

--- skillmatcher.py
+++ skillmatcher.py
@@ -15,9 +15,15 @@ def read_json_files(directory):
 def match_candidates_to_job(parsed_resumes, job_description):
     matched_candidates = []
     for resume in parsed_resumes:
-        if set(resume['skills']).issuperset(job_description['skills_required']):
-            if any(exp in resume['experience'] for exp in job_description['experience_required']):
-                matched_candidates.append(resume['name'])
+        if any(skill.lower() in [s.lower() for s in resume['skills']] for skill in job_description['skills_required']):
+            if any(exp.lower() in [e.lower() for e in resume['experience']] for exp in job_description['experience_required']):
+                if any(edu.lower() in [e[0].lower() for e in resume['education']] for edu in job_description['education_required']):
+                    if all(comp.lower() in [c.lower() for c in resume['competencies']] for comp in job_description['competencies_required']):
+                        matched_candidates.append(resume['name'])
+                    elif not job_description['competencies_required']:
+                        matched_candidates.append(resume['name'])
+                elif not job_description['education_required']:
+                    matched_candidates.append(resume['name'])
     return matched_candidates
 
 def main():
@@ -29,6 +35,7 @@ def main():
     job_description_data = {
         'title': 'Data Scientist',
         'skills_required': ['python', 'machine learning', 'data analysis'],
+        'education_required': ['BE','B.E.', 'B.E', 'BS', 'B.S', 'ME', 'M.E', 'M.E.', 'MS', 'M.S', 'BTECH', 'MTECH', 'SSC', 'HSC', 'CBSE', 'ICSE', 'X', 'XII' ],
         'experience_required': ['data science', 'machine learning'],
         'competencies_required': {'teamwork': ['collaboration', 'communication']},
         'measurable_results_expected': {'revenue growth': ['increase sales']}
